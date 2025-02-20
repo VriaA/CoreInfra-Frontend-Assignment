0b21fbd7ca3bf22a7bac7c8369494f2be2dda03f
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-02-20 10:04:27 +0100

    add welcome banner

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import DashboardHeader from "./components/DashboardHeader";
-
+import UserWelcomeBanner from "./components/UserWelcomeBanner";
 export default function Dashboard() {
   return (
     <div>
       <DashboardHeader />
-      <h1 className="font-satoshi">Dashboard</h1>
+      <main className="px-5 pt-2 pb-5">
+        <UserWelcomeBanner />
+      </main>
     </div>
   );
 }
