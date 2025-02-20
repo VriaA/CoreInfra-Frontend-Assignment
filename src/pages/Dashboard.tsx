05442a55892fddb64ed85cf82f61342a8297bcd9
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-02-20 10:58:22 +0100

    add quick access section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
 import DashboardHeader from "./components/DashboardHeader";
 import UserWelcomeBanner from "./components/UserWelcomeBanner";
+import QuickAccess from "./components/QuickAccess";
+
 export default function Dashboard() {
   return (
-    <div>
+    <div className="min-h-svh bg-[#F8FBFF]">
       <DashboardHeader />
       <main className="px-5 pt-2 pb-5">
         <UserWelcomeBanner />
+        <QuickAccess />
       </main>
     </div>
   );
---
+++ src/pages/components/QuickAction.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ChevronRight from "../../assets/icons/chevron_right.svg?react";
+import { Link } from "react-router";
+interface CardActionProps {
+  action: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  link: string;
+}
+
+export default function QuickAction({ action, Icon, link }: CardActionProps) {
+  return (
+    <Link
+      to={link}
+      className="flex h-[45px] w-full items-center gap-4 rounded-sm bg-[#f1f7ff] pl-4">
+      <span className="flex h-7 w-7 items-center justify-center rounded-full bg-[#014daf]">
+        <Icon
+          className="h-4 w-4 text-white"
+          stroke="white"
+        />
+      </span>
+      <span className="flex items-center gap-[5px]">
+        <span className="font-satoshi text-[14px] leading-[18px] font-medium text-[#121212]">
+          {action}
+        </span>
+        <ChevronRight />
+      </span>
+    </Link>
+  );
+}
