5e659e56e4c217af84e9b2a83514d8afe509eeb1
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-02-20 13:50:07 +0100

    adjust NavBar layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,36 +8,36 @@ import LapoLogo from "../assets/logos/lapo_logo.svg?react";
 
 export default function NavBar() {
   return (
-    <nav className="w-[230px] border-r border-[#dededf] bg-white">
-      <Link
-        to="."
-        aria-label="Lapo Microfinance Bank Logo"
-        className="mt-[27px] mb-[31px] ml-[10px] block">
-        <LapoLogo />
-      </Link>
+    <div className="w-[230px] border-r border-[#dededf] bg-white">
+      <nav className="mb-[165px]">
+        <Link
+          to="."
+          aria-label="Lapo Microfinance Bank Logo"
+          className="mt-[27px] mb-[31px] ml-[10px] block w-fit">
+          <LapoLogo />
+        </Link>
 
-      <NavBarLink
-        Icon={DashboardIcon}
-        name="Dashboard"
-        path="/"
-        className="mb-4 ml-[11px] block"
-      />
+        <NavBarLink
+          Icon={DashboardIcon}
+          name="Dashboard"
+          path="/"
+          className="mb-4 ml-[11px] block"
+        />
 
-      <section className="mb-[165px]">
-        <h2 className="font-satoshi mb-2 ml-7 text-[9px] leading-[18px] font-medium text-[#7e8b9c]">
-          MAIN MENU
-        </h2>
+        <section>
+          <h2 className="font-satoshi mb-2 ml-7 text-[9px] leading-[18px] font-medium text-[#7e8b9c]">
+            MAIN MENU
+          </h2>
 
-        <div className="ml-[11px] flex flex-col gap-1">
-          {navBarMenuItems.map((item) => (
-            <NavBarLink
-              key={item.path}
-              {...item}
-            />
-          ))}
-        </div>
-      </section>
-
+          <ul className="ml-[11px] flex flex-col gap-1">
+            {navBarMenuItems.map((item) => (
+              <li key={item.path}>
+                <NavBarLink {...item} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </nav>
       <button
         type="button"
         className="nav-link mb-11 ml-3 flex items-center gap-3 font-medium text-[#121212]">
@@ -56,6 +56,6 @@ export default function NavBar() {
         className="mb-[30px] ml-7"
         aria-label="cardinfra"
       />
-    </nav>
+    </div>
   );
 }
