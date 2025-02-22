041be321b790db06958cbd5d9680370a4229dcdb
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-02-22 11:39:54 +0100

    move statusStyles to constants directory

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -1,30 +1,9 @@
 interface StatusBadgeProps {
   status: "Ready" | "In Progress" | "Acknowledged" | "Pending";
 }
+import { statusStyles } from "../constants/statusStyles";
 
 export default function StatusBadge({ status }: StatusBadgeProps) {
-  const statusStyles = {
-    Ready: {
-      bg: "bg-[#ecfdf3]",
-      border: "border-[#abefc6]",
-      text: "text-[#067647]",
-    },
-    "In Progress": {
-      bg: "bg-[#fffaeb]",
-      border: "border-[#fedf89]",
-      text: "text-[#b54708]",
-    },
-    Acknowledged: {
-      bg: "bg-[#eff8ff]",
-      border: "border-[#b2ddff]",
-      text: "text-[#175cd3]",
-    },
-    Pending: {
-      bg: "bg-[#f9fafb]",
-      border: "border-[#eaecf0]",
-      text: "text-[#344054]",
-    },
-  };
   const style = statusStyles[status];
 
   return (
